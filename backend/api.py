d3a62e41968586b5fa0bdb1fc589fff479899a48
Miriam Goren <miriamgoren85@gmail.com>
2020-08-25 17:16:12 +0300

    MongoDB flask app connection

--- backend/api.py
+++ backend/api.py
@@ -7,22 +7,24 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
-# x = ''
-#
-# @app.before_first_request
-# def before_first_request():
-#     global x
-#     mongo = pymongo.MongoClient(
-#         "mongodb+srv://gigoAdmin:gigo1234@gigo.azdaq.mongodb.net/gigo?retryWrites=true&w=majority")
-#     gigo_DB = mongo["gigo"]
-#     gigo_COL = gigo_DB["locations"]
-#     x = gigo_COL.find_one()
+x = ''
+
+@app.before_first_request
+def before_first_request():
+    global x
+    mongo = pymongo.MongoClient(
+        "mongodb+srv://gigoAdmin:gigo1234@gigo.azdaq.mongodb.net/gigo?retryWrites=true&w=majority")
+    gigo_DB = mongo["gigo"]
+    gigo_COL = gigo_DB["locations"]
+    x = gigo_COL.find_one()
 
 
 @app.route('/')
 def hello_world():
-    # print(x)
-    return 'json.dumps(x)'
+    print(x)
+
+    resp = app.response_class(response=json.dumps({"dict":x['test']}), status=200, mimetype="application/json")
+    return resp
 
 
 if __name__ == "__main__":
--- client/src/components/MongoComponent.jsx
+++ client/src/components/MongoComponent.jsx
@@ -3,16 +3,16 @@ import React, { useEffect } from "react";
 const mongo = require("mongodb");
 
 const MongoComponent = () => {
-  const DB = "gigo";
-  const COLLECTION = "locations";
-  const MongoClient = mongo.MongoClient;
   const url =
     "mongodb+srv://gigoAdmin:gigo1234@gigo.azdaq.mongodb.net/gigo?retryWrites=true&w=majority";
 
   useEffect(() => {
     console.log("mounting");
-    const mongoResponse = await fetch('http://localhost:3000/')
-    });
+    const mongoResponse = fetch("http://localhost:5000/")
+      .then((resp) => resp.text())
+      .then((rest) => {
+        console.log(rest);
+      });
   }, []);
 
   return <div>MongoTesting</div>;
